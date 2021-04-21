e631d0a746d6fe8ee3ac2bb325e94b64d93038dc
dgladilov <d.gladilov005@gmail.com>
2021-04-21 15:15:03 +0300

    first commit

---
+++ Focus/Focus/Controllers/TimerVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Focus
+//
+//  Created by Dmitry Gladilov on 15.04.2021.
+//
+
+import UIKit
+
+class TimerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .cyan
+    }
+
+
+}
